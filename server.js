0087f7e559a6c78390a8c61ecc43d93cd4f62172
Gabriel Farrell <g@grrawr.com>
2014-05-31 01:28:35 -0400

    Cloning and pulling

--- server.js
+++ server.js
@@ -1,40 +1,53 @@
 var exec = require('child_process').exec;
+var fs = require('fs');
 var http = require('http');
+var path = require('path');
 var qs = require('querystring');
 var url = require('url');
 
-var port = process.argv[2] || 8951;
-var sites = ['wavefarm.org', 'phlccs.grrawr.com'];
+var port = process.env.PORT || 8951;
+var cwd = process.cwd()
 
 http.createServer(function (req, res) {
-  var urlParsed = url.parse(req.url);
-  if (urlParsed.pathname == '/') {
+  console.log(req.method, req.url);
+  var u = url.parse(req.url);
+  if (u.pathname == '/') {
     return res.end('ok\n');
   } 
-  var siteIndex = sites.indexOf(urlParsed.pathname.substr(1));
-  if (siteIndex != -1 && req.method == 'POST') {
+  var site = u.pathname.substr(1);
+  if (req.method == 'POST') {
     var body = '';
-    req.on('data', function (chunk) {body = body + chunk});
+    req.on('data', function (chunk) {body += chunk});
     req.on('end', function () {
-      var payload = JSON.parse(qs.parse(body).payload);
+      var payload = JSON.parse(body);
+      console.log(payload)
       if (payload.commits.length > 0) {
         for (var i=0; i<payload.commits.length; i++) {
           var commit = payload.commits[i];
         }
-        console.log(__dirname + '/' + sites[siteIndex]);
-        exec('git pull --rebase && git push -f ploy', {cwd: __dirname + '/' + sites[siteIndex]},
-          function (err, stdout, stderr) {
-            if (err) throw err;
-            console.log(stdout);
-            console.log(stderr);
+        var siteDir = path.resolve(cwd, site);
+        console.log(siteDir);
+        fs.exists(siteDir, function (exists) {
+          if (exists) {
+            exec('git pull', {cwd: siteDir}, function (err, stdout, stderr) {
+              if (err) throw err;
+              console.log(stdout);
+              console.error(stderr);
+              // TODO Stop server.js in this directory
+              // TODO Start server.js in this directory
+            });
+          } else {
+            exec('git clone ' + payload.repository.url + '.git', function (err, stdout, stderr) {
+              if (err) throw err;
+              console.log(stdout);
+              console.error(stderr);
+              // TODO Start server.js in this directory
+            });
           }
-        );
+        });
       }
       res.end('ok\n');
     });
-  } else {
-    res.writeHead(404, {'Content-Type': 'text/plain'});
-    res.end('404');
   }
 }).listen(port, function () {
   console.log('Running on port', port);

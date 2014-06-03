ef1bb5f27e2d8eef542ad8ed6492b037e9527ac9
Gabriel Farrell <g@grrawr.com>
2014-06-03 01:14:15 -0400

    Server starting and restarting

--- server.js
+++ server.js
@@ -1,12 +1,36 @@
-var exec = require('child_process').exec;
+var cp = require('child_process');
 var fs = require('fs');
 var http = require('http');
 var path = require('path');
-var qs = require('querystring');
 var url = require('url');
 
+
+var cwd = process.cwd();
 var port = process.env.PORT || 8951;
-var cwd = process.cwd()
+var servers = {};
+
+function startServer (name) {
+  var dir = path.resolve(cwd, name);
+  servers[name] = {
+    dir: dir,
+    proc: cp.fork(path.resolve(dir, 'server.js'), {cwd: dir, env: process.env})
+  };
+}
+
+// Run server.js in each directory
+fs.readdirSync(cwd).filter(function (file) {
+  return fs.statSync(file).isDirectory()
+}).forEach(startServer);
+
+// Handler for exec calls
+function hx (cb) {
+  return function (err, stdout, stderr) {
+    if (err) throw err;
+    console.log(stdout);
+    console.error(stderr);
+    if (cb) cb();
+  };
+}
 
 http.createServer(function (req, res) {
   console.log(req.method, req.url);
@@ -21,30 +45,19 @@ http.createServer(function (req, res) {
     req.on('end', function () {
       var payload = JSON.parse(body);
       console.log(payload)
-      if (payload.commits.length > 0) {
-        for (var i=0; i<payload.commits.length; i++) {
-          var commit = payload.commits[i];
-        }
-        var siteDir = path.resolve(cwd, site);
-        console.log(siteDir);
-        fs.exists(siteDir, function (exists) {
-          if (exists) {
-            exec('git pull', {cwd: siteDir}, function (err, stdout, stderr) {
-              if (err) throw err;
-              console.log(stdout);
-              console.error(stderr);
-              // TODO Stop server.js in this directory
-              // TODO Start server.js in this directory
-            });
-          } else {
-            exec('git clone ' + payload.repository.url + '.git', function (err, stdout, stderr) {
-              if (err) throw err;
-              console.log(stdout);
-              console.error(stderr);
-              // TODO Start server.js in this directory
-            });
-          }
-        });
+      if (servers[site]) {
+        cp.exec('git pull', {cwd: servers[site].dir}, hx(function () {
+          servers[site].proc.once('exit', function () {startServer(site)});
+          servers[site].proc.kill();
+        }));
+      } else {
+        if (!payload.repository) return res.end('no repo in payload\n');
+        cp.exec('git clone ' + payload.repository.url + '.git', hx(function () {
+          var dir = path.resolve(cwd, site);
+          cp.exec('npm install', {cwd: dir}, hx(function () {
+            startServer(site);
+          }));
+        }));
       }
       res.end('ok\n');
     });
